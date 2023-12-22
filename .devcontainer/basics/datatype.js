b84da379e5bb6d80fdd491b49f176a37c2112d31
Yogesh Pandey <bc21143@smsvaranasi.in>
2023-12-22 18:16:33 +0530

    datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict";  // treate all js code as newe version
+
+
+let name = "yogesh"
+let age = 18
+let isLoggedIn =false
+
+// number 2^53
+// bigInt
+//string  ""
+//boolean true false
+//null standAlone value
+//undefined 
+// symbol
+
+
+
+//objects
+
+console.log(typeof "yogesh");
+console.log(typeof undefined);
--- .devcontainer/basics/text.js
+++ .devcontainer/basics/text.js
@@ -1,1 +1,10 @@
-console.log("yogesh pandey");
+console.log("yogesh pandey")
+const accountId=144553;
+let accountEmail ="yogigmail.com"
+var accountPassword ="45678"
+accountCity ="jaipur";
+// accountId=2;
+accountEmail ="ymadfghjkil.com"
+accountPassword ="48"
+accountCity ="banglore"
+console.table([accountId,accountPassword,accountEmail,accountCity])
