1878ffa23644ff5e8cb49dc03080ac5ec93adcad
Yogesh Pandey <bc21143@smsvaranasi.in>
2023-12-22 18:53:59 +0530

    operation

---
+++ .devcontainer/basics/operation.js
@@ -0,0 +1,27 @@
+let value = 3
+let negValue =-value
+
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "yogesh"
+
+let str3 = str1+str2; // ->> helloyogesh
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+ "3");
+
+console.log("1"+2 +4);   //string+ number-> string
+console.log(1+2 +"4");     // number + string -> first operate then 
+
+console.log(+true);     
